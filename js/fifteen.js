3d7a3848af36f8658fa1c0b19902cd9fca0ee73c
Kostiantyn Kvitkin <kosta_kit@ukr.net>
2016-11-27 18:36:35 +0200

    Зміна концепції виводу поля. тепер розмір поля фіксований 

--- js/fifteen.js
+++ js/fifteen.js
@@ -1,100 +1,192 @@
 'use strict';
 
-function FifteenConsole() {
-  this._field = [[1, 2, 3, 4], 
-                 [5, 6, 7, 8], 
-                 [9, 10, 11, 12],
-                 [13, 14, 15, 0]];
-  this.mix();
-}
+/*  exports in the returned object: 
+      (classes: FifteenConsole, FifteenGraphic, FifteenGraphicAnyFieldSize;)
+      debugOn 
+*/
 
-FifteenConsole.prototype.draw = function() {
-  console.log(this._field[0], '\n', 
-              this._field[1], '\n', 
-              this._field[2], '\n', 
-              this._field[3]);
-};
 
-FifteenConsole.prototype._exchangeCells = function(row1, col1, row2, col2) {
-  let temp;
-  temp =  this._field[row1][col1];
-  this._field[row1][col1] = this._field[row2][col2];
-  this._field[row2][col2] = temp;
-};
+var fifteenModelModule = (function(){
+  var f = {  };
+  f.debugOn = true;
+  // f.debugOn = false;
 
-FifteenConsole.prototype._findNumber = function(num) {
-  for (let row = 0; row <= 3; row++) {
-    for (let col = 0; col <= 3; col++) {
-      if (this._field[row][col] == num) return [row, col];
+  function FifteenConsole() {
+    this._field = [[1, 2, 3, 4],              //start fill field 
+                   [5, 6, 7, 8], 
+                   [9, 10, 11, 12],
+                   [13, 14, 15, 0]];
+    // this.mix();                            //field mixing
+  }
+
+  FifteenConsole.prototype.draw = function() {
+    if (f.debugOn) {
+      console.log(this._field[0], '\n', 
+                  this._field[1], '\n', 
+                  this._field[2], '\n', 
+                  this._field[3]);
+    }
+  };
+
+    //exchange cell numbers
+  FifteenConsole.prototype._exchangeCells = function(row1, col1, row2, col2) {
+    let temp =  this._field[row1][col1];                          
+    this._field[row1][col1] = this._field[row2][col2];
+    this._field[row2][col2] = temp;
+  };
+
+    //find cell with number
+  FifteenConsole.prototype._findNumber = function(num) {
+    for (let row = 0; row <= 3; row++) {
+      for (let col = 0; col <= 3; col++) {
+        if (this._field[row][col] == num) return [row, col]; //!!return array
+      }
+    }
+  };
+    
+    //generate random number [min, max]
+  FifteenConsole.prototype._getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+    //field mixing
+  FifteenConsole.prototype.mix = function() {
+
+    if (f.debugOn) {
+      console.log('%cExchanges cells', 
+        "color: yellow; font-size: 2em; font-style: bold; background-color: blue; padding: 2px;");
+    }
+
+      //field mixing
+    let rowTemp1, colTemp1, rowTemp2, colTemp2;
+    for (let i = 0; i < 100; i++) {
+      rowTemp1 = this._getRandomInt(0, 4);
+      colTemp1 = this._getRandomInt(0, 4);
+      rowTemp2 = this._getRandomInt(0, 4);
+      colTemp2 = this._getRandomInt(0, 4);
+
+      if (f.debugOn) {
+        console.log('cell1: ' + rowTemp1 + ', ' + colTemp1 + '  cell2: ' + rowTemp2 + ', ' + colTemp2);
+        this._exchangeCells(rowTemp1, colTemp1, rowTemp2, colTemp2);
+      }
+    }
+    this.draw();
+  };
+
+    //exchange a cell with number and an hole cell
+  FifteenConsole.prototype.moveNumber = function(num) {
+    let rowNum, colNum, tempNum;
+    let rowZero, colZero, tempZero;
+    tempNum = this._findNumber(num);
+    rowNum = tempNum[0]; colNum = tempNum[1];
+
+    tempZero = this._findNumber(0);
+    rowZero = tempZero[0]; colZero = tempZero[1];
+
+
+    if ( ((rowNum == rowZero) && ( ((colNum - colZero) == 1) || ((colZero - colNum) == 1) )) ||
+         ((colNum == colZero) && ( ((rowNum - rowZero) == 1) || ((rowZero - rowNum) == 1) ))
+      ) {
+
+      this._exchangeCells(rowNum, colNum, rowZero, colZero);
+      this.draw();
+   } else { if (f.debugOn) { console.log ('Error'); } }
+  };
+
+  /*          New Class                   */
+
+  function FifteenGraphic() { //extends FifteenConsole
+    FifteenConsole.apply(this, arguments);
+    this.field = this._field;
+  }
+
+  FifteenGraphic.prototype = Object.create(FifteenConsole.prototype);
+  FifteenGraphic.prototype.constructor = FifteenGraphic;
+
+  class FifteenGraphicAnyFieldSize extends FifteenGraphic{
+    constructor(){
+      super();
+      delete this.field;
+      delete this._field;
+  }
+
+    init(rowSize, colSize){
+      rowSize = rowSize || 4;
+      colSize = colSize || 4;
+
+      let field = [];
+      let num = 0;
+      for (let i = 0; i < rowSize; i++) {
+        field[i] = [];
+        for (let j = 0; j < colSize; j++) {
+          field[i][j] = num++;
+        } 
+      }
+
+      this.field = field;
+      this._field = this.field;
+      this.rowSize = rowSize;
+      this.colSize = colSize;
+
+      this.mix();
+      this.draw();
+    }
+
+    toString() {
+      let field = this.field;
+      let str ='';
+      for (let i = 0; i < this.rowSize; i++) {
+        for (let j = 0; j < this.colSize; j++) {
+          str += this.field[i][j] + ' ';
+        } 
+
+        str += '\n';
+      }
+      return str;
+    }
+
+    _findNumber(num) {
+      for (let row = 0; row < this.rowSize; row++) {
+        for (let col = 0; col < this.colSize; col++) {
+          if (this._field[row][col] == num) return [row, col]; //!!return array
+        }
+      }
+    }
+
+    mix() {
+
+      if (f.debugOn) {
+        console.log('%cExchanges cells', 
+          "color: yellow; font-size: 2em; font-style: bold; background-color: blue; padding: 2px;");
+      }
+
+        //field mixing
+      let rowTemp1, colTemp1, rowTemp2, colTemp2;
+      for (let i = 0; i < this.rowSize*this.colSize*6; i++) {
+        rowTemp1 = this._getRandomInt(0, this.rowSize);
+        colTemp1 = this._getRandomInt(0, this.colSize);
+        rowTemp2 = this._getRandomInt(0, this.rowSize);
+        colTemp2 = this._getRandomInt(0, this.colSize);
+
+        if (f.debugOn) {
+          console.log('cell1: ' + rowTemp1 + ', ' + colTemp1 + '  cell2: ' + rowTemp2 + ', ' + colTemp2);
+          this._exchangeCells(rowTemp1, colTemp1, rowTemp2, colTemp2);
+        }
+      }
+      this.draw();
+    }
+
+    draw() {
+      if (f.debugOn) {
+        console.log(this.toString());
+      }
     }
   }
-};
 
-
-FifteenConsole.prototype.mix = function() {
-
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  console.log('%cExchanges cells', 
-    "color: yellow; font-size: 2em; font-style: bold; background-color: blue; padding: 2px;");
-
-  let rowTemp1, colTemp1, rowTemp2, colTemp2;
-  for (let i = 0; i < 100; i++) {
-    rowTemp1 = getRandomInt(0, 4);
-    colTemp1 = getRandomInt(0, 4);
-    rowTemp2 = getRandomInt(0, 4);
-    colTemp2 = getRandomInt(0, 4);
-
-    console.log('cell1: ' + rowTemp1 + ', ' + colTemp1 + '  cell2: ' + rowTemp2 + ', ' + colTemp2);
-    this._exchangeCells(rowTemp1, colTemp1, rowTemp2, colTemp2);
-  }
-  this.draw();
-};
-
-
-FifteenConsole.prototype.moveNumber = function(num) {
-  let rowNum, colNum, tempNum;
-  let rowZero, colZero, tempZero;
-  tempNum = this._findNumber(num);
-  rowNum = tempNum[0]; colNum = tempNum[1];
-
-  tempZero = this._findNumber(0);
-  rowZero = tempZero[0]; colZero = tempZero[1];
-
-
-  if ( ((rowNum == rowZero) && ( ((colNum - colZero) == 1) || ((colZero - colNum) == 1) )) ||
-       ((colNum == colZero) && ( ((rowNum - rowZero) == 1) || ((rowZero - rowNum) == 1) ))
-    ) {
-
-    this._exchangeCells(rowNum, colNum, rowZero, colZero);
-    this.draw();
- } else { console.log ('Error');}
-};
-
-/*          New Class                   */
-
-function FifteenGraphic() {
-  FifteenConsole.apply(this, arguments);
-  this.field = this._field;
-}
-
-FifteenGraphic.prototype = Object.create(FifteenConsole.prototype);
-FifteenGraphic.prototype.constructor = FifteenGraphic;
-
-FifteenGraphic.prototype.winField1 = [[1, 2, 3, 4], 
-                                      [5, 6, 7, 8], 
-                                      [9, 10, 11, 12],
-                                      [13, 14, 15, 0]];
-FifteenGraphic.prototype.winField2 = [[1, 2, 3, 4], 
-                                      [5, 6, 7, 8], 
-                                      [9, 10, 11, 12],
-                                      [13, 14, 15, 0]];
-
-/*FifteenGraphic.prototype.isWin = function() {
-  for (let row = 0; row <= 3; row++) {
-    for (let col = 0; col <= 3; col++) {
-}*/
-
+  f.FifteenConsole = FifteenConsole;
+  f.FifteenGraphic = FifteenGraphic;
+  f.FifteenGraphicAnyFieldSize = FifteenGraphicAnyFieldSize;
+  
+  return f;
+})();
 
--- js/interface_f.js
+++ js/interface_f.js
@@ -1,54 +1,299 @@
 'use strict';
 
-var fifteen; 
-var fieldBox = [];
-onload = function() {
+/*  exports in the returned object: 
+      (classes: )
+       
+*/
 
-  fieldBox[0] = document.getElementsByClassName('text 0');
-  fieldBox[1] = document.getElementsByClassName('text 1');
-  fieldBox[2] = document.getElementsByClassName('text 2');
-  fieldBox[3] = document.getElementsByClassName('text 3');
-  // console.log(fieldBox);
+var fifteenAppModule = (function(){
+  var a = {  };
+  a.debugOn = true;
+  // a.debugOn = false;
+  
+  fifteenModelModule.debugOn = a.debugOn;
 
-  fifteen =new FifteenGraphic();
-  draw();
-};
+  // a.debugState = 'choiceSize';
+  // a.debugState = 'stateGame';
 
-field.onclick = function(event) {
-  // console.log(+event.target.textContent);
-  let num =  +event.target.textContent;     //if "cell" touch
-  if ( !isNaN(num) ) {                      // 
-    console.log(num);
-    fifteen.moveNumber(num);
-    draw();
+
+class FifteenApp {
+
+  constructor(viewer, model) {
+    this.viewer = viewer;
+    this.model = model;
+    this.viewer.init();
+
+    this.SIZEMAX = 20;
+    this.SIZEMIN = 2;
+
+    this.RUNGAME = 'runGame';
+    this.CHOICESIZE = 'choiceSize';
+    this.CREATEFIELDGAME = 'createFieldGame';
+    this.DESTROYFIELDGAME = 'destroyFieldGame';
+    this.state = this.CHOICESIZE;
+
+    this.draw();
   }
-};
 
-function draw() {
-  for (let row = 0; row <= 3; row++) {
-    for (let col = 0; col <= 3; col++) {
-      if (fifteen.field[row][col]) {
-        fieldBox[row][col].parentElement.className = 'cell';
-        fieldBox[row][col].textContent = fifteen.field[row][col];
-      } else {
-        fieldBox[row][col].parentElement.className = 'hole';
-        fieldBox[row][col].parentElement.className
-        fieldBox[row][col].textContent = '';
+  get state() { return this._state;}
+  set state(newState) { this._state = newState;}
 
-      }
+  init(rowSize, colSize) {
+    if (rowSize >= this.SIZEMIN && rowSize <= this.SIZEMAX &&
+        colSize >= this.SIZEMIN && colSize <= this.SIZEMAX) {
       
+
+      this.rowSize = rowSize;
+      this.colSize = colSize;
+
+      this.model.init(rowSize, colSize);
+      this.state = this.CREATEFIELDGAME;
+
+      this.draw();//?
+
+      this.state = this.RUNGAME;
+      this.draw();
+    }
+  }
+
+  reset() {
+    this.state = this.DESTROYFIELDGAME;
+    this.rowSize = undefined;
+    this.colSize = undefined;
+
+    this.draw(); //?
+
+    this.state = this.CHOICESIZE;
+    this.draw();
+
+  }
+
+
+  draw() {
+    if (a.debugOn) console.log(this.state);
+    switch (this.state) {
+      case this.RUNGAME: this.viewer.gameShow();
+      break;
+      case this.CHOICESIZE: this.viewer.choiceSizeShow();
+      break;
+      case this.CREATEFIELDGAME: this.viewer.createFieldGame(this.rowSize, this.colSize);
+      break;
+      case this.DESTROYFIELDGAME: this.viewer.destroyFieldGame();
+      break;
     }
   }
 
 
-  
+  setSize(){
+    let rowSize = +document.getElementById('txtSizeVertical').value;
+    let colSize = +document.getElementById('txtSizeHorizontal').value;
+
+    if (a.debugOn) console.log(rowSize, colSize);
+    
+    this.init(rowSize, colSize);
+    
+  }
+
+  touchCell(event) {
+    if (a.debugOn) console.log(event.target.id);
+
+    // let num =  +event.target.textContent;     //if "cell" touch
+    // if ( !isNaN(num) ) {                      // 
+    //   console.log(num);
+    //   fifteen.moveNumber(num);
+    //   draw();
+    // }
+
+
+  }
+
 }
 
-// document.getElementById('field 15').onclick=function(){
-//   console.log(15);
+
+
+class Views {
+  static init() {
+    this.inputSizeDiv = document.getElementById('inputSize');
+    this.gameDiv = document.getElementById('game');
+    this.fieldGameBoxDiv = document.getElementById('fieldGameBox');
+    this.fieldGameDiv = undefined;    //created dynamically
+
+    /*this.primarySizes = {
+      // small: 10,
+      // big: 20,
+      rowSwitchSize: 8,
+      colSwitchSize: 13,
+      // shapefactor: 1.1
+    };*/
+
+    this.baseSize = undefined;      //calculated dynamically
+    this.cellSize = 3;
+    this.gap = 0.08;
+    this.borderRadius = 0.3;
+    this.fontSize = 1.5;
+
+  }
+
+  static choiceSizeShow() {
+    this.inputSizeDiv.hidden = false;
+    this.gameDiv.hidden = true;
+    
+
+  }
+  static gameShow() {
+    this.inputSizeDiv.hidden = true;
+    this.gameDiv.hidden = false; 
+
+  }
+
+  static _calcSize(rowSize, colSize) {
+    // console.log(this.fieldGameBoxDiv.style);height
+      let divWidth = +(getComputedStyle(this.fieldGameBoxDiv).width.slice(0,-2));
+      let divHeight = +(getComputedStyle(this.fieldGameBoxDiv).height.slice(0,-2));
+      let baseSizeWidth = divWidth/(colSize*(this.cellSize + this.gap) + 1);
+      let baseSizeHeight = divHeight/(rowSize*(this.cellSize + this.gap) + 1);
+
+      console.log(baseSizeWidth, baseSizeHeight);
+
+      // return baseSize;
+
+  }
+
+
+  static createFieldGame(rowSize, colSize) {
+
+    this._calcSize(rowSize, colSize);
+    return;
+
+
+
+    if (rowSize <= this.primarySizes.rowSwitchSize && colSize <= this.primarySizes.colSwitchSize) {
+      this.baseSize = this.primarySizes.big;
+    } else {
+      this.baseSize = this.primarySizes.small;
+    }
+
+    let baseSize = this.baseSize;
+    let cellSize = this.cellSize;
+    let gap = this.gap;
+    let borderRadius = this.borderRadius;
+    let fontSize = this.fontSize;
+
+
+    let tempFieldGameDiv = document.createElement('div');
+    tempFieldGameDiv.id = 'fieldGame';
+
+    tempFieldGameDiv.style.width = colSize*(cellSize*baseSize + gap*baseSize) + gap*baseSize + 'px';
+    tempFieldGameDiv.style.height = rowSize*(cellSize*baseSize + gap*baseSize) + gap*baseSize + 'px';
+
+
+
+    let tempHTML = '';
+    for (let i = 0; i < rowSize; i++) {
+      for (let j = 0; j < colSize; j++) {
+        tempHTML += '<div class = "cell" style = "';
+        tempHTML += 'left: ' + (gap*baseSize + j*(cellSize*baseSize + gap*baseSize)) + 'px; ';
+        tempHTML += 'top: ' + (gap*baseSize + i*(cellSize*baseSize + gap*baseSize)) + 'px; ';
+        tempHTML +=  'width: ' + cellSize*baseSize + 'px; height: ' + cellSize*baseSize + 'px; ';
+        tempHTML += '"';
+
+        tempHTML += ' id = "c' + i + j + '"> ' + /*i + j +*/ ' </div>';
+      }
+    }
+
+    tempFieldGameDiv.innerHTML = tempHTML;
+
+    let cellsCollection = tempFieldGameDiv.getElementsByClassName('cell');
+    [].forEach.call(cellsCollection, function(cell) {
+      cell.style.borderRadius = borderRadius*baseSize + 'px';
+      let tempP = document.createElement('p');
+      tempP.id = 'p' + cell.id.slice(1);
+      tempP.className = 'p';
+      tempP.style.fontSize = fontSize*baseSize + 'px';
+      tempP.innerText = cell.id.slice(1);
+      cell.appendChild(tempP);
+    });
+
+
+
+    this.fieldGameDiv = this.fieldGameBoxDiv.appendChild(tempFieldGameDiv);
+
+    this.inputSizeDiv.hidden = true;
+    this.gameDiv.hidden = false; 
+  }
+
+  static destroyFieldGame() {
+    this.fieldGameBoxDiv.removeChild(this.fieldGameDiv);
+
+    this.inputSizeDiv.hidden = true;
+    this.gameDiv.hidden = true; 
+  }
+
+  // static draw() {
+
+  // }
+}
+
+
+
+// var fifteen; 
+
+// var fieldBox = [];
+// onload = function() {
+
+//   fieldBox[0] = document.getElementsByClassName('text 0');
+//   fieldBox[1] = document.getElementsByClassName('text 1');
+//   fieldBox[2] = document.getElementsByClassName('text 2');
+//   fieldBox[3] = document.getElementsByClassName('text 3');
+//   // console.log(fieldBox);
+
+//   // fifteen = new FifteenGraphic();
+//   // draw();
+
+//   // fifteen = new FifteenGraphicAnyFieldSize();
+//   // fifteen.init(5, 5);
+//   // console.log(fifteen.toString());
+
 // };
 
-// document.getElementById('fieldBox').onclick = function() {
-//   console.log('fieldBox');
+// field.onclick = function(event) {
+//   // console.log(+event.target.textContent);
+//   let num =  +event.target.textContent;     //if "cell" touch
+//   if ( !isNaN(num) ) {                      // 
+//     console.log(num);
+//     fifteen.moveNumber(num);
+//     draw();
+//   }
 // };
 
+// function draw() {
+//   for (let row = 0; row <= 3; row++) {
+//     for (let col = 0; col <= 3; col++) {
+//       if (fifteen.field[row][col]) {
+//         fieldBox[row][col].parentElement.className = 'cell';
+//         fieldBox[row][col].textContent = fifteen.field[row][col];
+//       } else {
+//         fieldBox[row][col].parentElement.className = 'hole';
+//         // fieldBox[row][col].parentElement.className;
+//         fieldBox[row][col].textContent = '';
+
+//       }
+      
+//     }
+//   }
+
+
+  
+// }
+
+  a.fApp = new FifteenApp(Views, new fifteenModelModule.FifteenGraphicAnyFieldSize());
+
+
+  choiceSizeButton.onclick = a.fApp.setSize.bind(a.fApp);
+  resetButton.onclick = a.fApp.reset.bind(a.fApp);
+  game.onclick = a.fApp.touchCell.bind(a.fApp);
+
+
+  
+  return a;
+})();
